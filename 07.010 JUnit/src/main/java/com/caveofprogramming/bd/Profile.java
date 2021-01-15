bc556be4caf3f30d156e44196080c764feaf72ab
TsholoMash <mashianeshirley27@gmail.com>
2021-01-15 17:32:41 +0200

    Created

---
+++ 07.010 JUnit/src/main/java/com/caveofprogramming/bd/Profile.java
@@ -0,0 +1,27 @@
+package com.caveofprogramming.bd;
+
+import java.util.Properties;
+
+public class Profile {
+	
+	public static Properties getProperties(String name) {
+		
+		Properties props = new Properties();
+    	
+    	String env = System.getProperty("env");
+    	
+    	if(env == null) {
+    		env = "dev";
+    	}
+    	
+    	String propertiesFile = String.format("/config/%s.%s.properties", name, env);
+    	
+    	try {
+			props.load(App.class.getResourceAsStream(propertiesFile));
+		} catch (Exception e1) {
+			throw new RuntimeException("Cannot load properties file: " + propertiesFile);
+		}
+    	
+    	return props;
+	}
+}
---
+++ 07.010 JUnit/src/main/java/com/caveofprogramming/bd/UserDao.java
@@ -0,0 +1,5 @@
+package com.caveofprogramming.bd;
+
+public interface UserDao  extends Dao<User>{
+
+}
