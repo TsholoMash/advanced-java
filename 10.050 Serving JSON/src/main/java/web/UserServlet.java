7e17b4457f9d52c6506d3d6d68db9ad9501c4080
TsholoMash <mashianeshirley27@gmail.com>
2021-02-20 22:56:35 +0200

    Created

---
+++ 10.050 Serving JSON/src/main/java/web/UserServlet.java
@@ -0,0 +1,54 @@
+package web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<User> users = List.of(new User("Neptune", "Hello"), new User("Morpheus", "xyz"));
+		
+		Gson gson = new Gson();
+		
+		String json = gson.toJson(users);
+		System.out.println(json);
+		
+		response.setContentType("application/json");
+		
+		response.getWriter().println(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
